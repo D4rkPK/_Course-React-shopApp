a3adad917770020efe5945f42624de6ae08fc327
bjpc <brandonpineda4@gmail.com>
2022-05-04 13:10:03 -0600

    :art: [ADD] remove products from cart

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -5,18 +5,26 @@ const initialState = {
 }
 
 const useInitialSate = () => {
-    const [state, steState] = useState(initialState);
+    const [state, setState] = useState(initialState);
 
     const addToCart = (payload) => {
-        steState({
+        setState({
             ...state,
             cart: [...state.cart, payload]
         })
     }
 
+    const removeFromCart = (payload) => {
+        setState({
+            ...state,
+            cart: state.cart.filter(items => items.id !== payload.id),
+        });
+    }
+
     return {
         state, 
-        addToCart
+        addToCart,
+        removeFromCart
     }
 }
 
